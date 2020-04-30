bc456531425bdeefdc004178ac0b8aae3cdb6355
Jay Hong <aquahjh1@gmail.com>
2020-04-30 12:43:32 -0400

    Data models

---
+++ JS_Values&Variables.js
@@ -0,0 +1,57 @@
+// Premitive Types
+
+// Number - there is only one type of number type: Number
+// Positive, negative, whole number and decimal etc., 
+//  /(divide) +(addition) -(subtraction) %(mod) ** (power)
+
+// NaN (Not a number) represents something that is not a number
+// 0/0 -> NaN (infinity and -infinity exist,  0 and -0 as well)  
+
+
+// String 
+// both doulbe and single quote can be used 
+// typeof "87" -> "string"
+// we can concatenate strings, using + 
+// property is just a value that we can access 
+// "hello".length 5 (length is property): This means that String is an object (IMO) 
+// Strings are immutable 
+
+// String escapes 
+// \' (single quote) \" (double quote) \n (new line) \\ (backslash)
+
+// String Template Literals 
+// Template literals are strings that allow embedded expressions, which will be evaluated 
+// and then turned into a resulting string 
+// `${1+5}` This only works with back-tick (` -> above tap key)
+
+
+// Boolean
+// true or false  (1 or 0, yes or no) 
+
+// Interestingly, variables types can be modified in JS 
+// Ex) let a = true; and writing a = 3 will work; 
+
+// null - intentional absence of any value 
+// Undefiend  - variables that do not have an assigned value are undefiend
+
+
+// Variables
+// let someName = value; -> assigning a value to a variable 
+// if a varialbe is created with let, a variable with the same name cannot be declared 
+// ex) let a = 3 and writing let a = 4 right after does not make sense 
+
+// const -> constant 
+// You cannot changer the values declared with const 
+
+// Math Object - contains properties and methods for mathematical constants and functions 
+// typeof null = "object" Remember! 
+
+// parseInt('24') = 24 parseFloat('24.987') = 24.987
+// parseInt('$99') -> NaN parseInt('99cents') -> 99
+// If the first charactor is not a number, it returns NaN 
+// but if non interger tyoe string values come after, it returns the numbers before that charactor 
+
+// Changed string to integer or float 
+
+
+
