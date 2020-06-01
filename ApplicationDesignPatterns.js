be92c4cc3302016bf7bd19f08aaa4aa9242c81be
Jay Hong <aquahjh1@gmail.com>
2020-05-31 21:20:38 -0400

    ApplicationDesignPattern

--- ApplicationDesignPatterns.js
+++ ApplicationDesignPatterns.js
@@ -13,21 +13,45 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class = "input" />
+    <div class = "dropdown">
+        <div class = "dropdown-menu">
+            <div class = "dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input'); 
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
     for (let movie of movies){
-        const div = document.createElement('div');
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-        div.innerHTML = `
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
             <img src = "${movie.Poster}"/>
-            <h1>${movie.Title}</h1>
+            ${movie.Title}
         `;
-
-        document.querySelector('#target').appendChild(div);
+        resultsWrapper.appendChild(option);
     }
 };
 
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+})
