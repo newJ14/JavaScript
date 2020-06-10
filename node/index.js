baff4c67f2b6f195bdbbe0418e83bc47e8cf37e7
Jay Hong <aquahjh1@gmail.com>
2020-06-09 23:53:39 -0400

    NodeJS

---
+++ node/index.js
@@ -0,0 +1,42 @@
+// const counterObject = require('./myscript.js')
+
+// console.log(counterObject.getCounter());
+// counterObject.incrementCounter();
+// console.log(counterObject.getCounter());
+
+
+// const newCounterObject = require('./myscript.js')
+// console.log(newCounterObject.getCounter());
+
+// Debugging
+// c - continue excution until program ends or next debugger statement
+// n - RUn the next line of code
+// s - Step in to a function
+// o - Step out of the current function
+
+// node inspect index.js -> Start up a debugger CLI and pause execution whenever a 'debugger' statement is hit 
+// node --inspect-brk index.js -> Start up a debugger instance and wait to execute until a debugger is connected. Access the debugger at 'chrome://inspect'
+
+//repl -> start up an execution environment so we can refer to different variables in the program
+
+
+
+// Accessing Standard Library Module 
+
+const fs = require('fs');
+
+// can use process.cwd() without require statement 
+fs.readdir(process.cwd(), (err, files) => {
+    // Either 
+    // err === an error object, which means something went wrong
+    // err === null, which means everything is ok 
+
+    if(err) {
+        //error handling code here
+        // throw new Error(err);
+        console.log(err);
+    }
+
+    console.log(files);
+});
+
---
+++ node/myscript.js
@@ -0,0 +1,13 @@
+// let counter = 0 ;
+// // since the file is only ran one time, no matter how many times the user run index.js, the counter variable is not going to be called again
+
+// module.exports = {
+//     incrementCounter(){
+//         counter = counter + 1;
+//     }, 
+//     getCounter(){
+//         return counter;
+//     }
+// };
+
+
