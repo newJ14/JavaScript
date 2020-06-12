0985e928475661dcbd3c3f65279aa710f4def468
Jay Hong <aquahjh1@gmail.com>
2020-06-12 00:27:18 -0400

    searchDir

--- node/index.js
+++ node/index.js
@@ -3,9 +3,22 @@
 // Accessing Standard Library Module 
 
 const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
+
+
+
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
 
 // can use process.cwd() without require statement 
-fs.readdir(process.cwd(), (err, files) => {
+fs.readdir(targetDir, async (err, files) => {
     // Either 
     // err === an error object, which means something went wrong
     // err === null, which means everything is ok 
@@ -16,31 +29,74 @@ fs.readdir(process.cwd(), (err, files) => {
         console.log(err);
     }
 
-    const allStats = Array(files.length).fill(null);
+    const statPromises = files.map(file => {
+        return lstat(path.join(targetDir, file));
+    });
 
-    for (let file of files){
-        const index = files.indexOf(file)
-        fs.lstat(file, (err, stats) => {
-            if(err){
-                console.log(err);
-            }
+    const allStats = await Promise.all(statPromises);
 
-            allStats[index] = stats; 
+    for(let stats of allStats) {
+        const index = allStats.indexOf(stats);
 
-            const ready = allStats.every((stats) => {
-                return stats     
-            });
+        if(stats.isFile()){
+            console.log(files[index])
+        } else {
+            console.log(chalk.bold(files[index]));
 
-            if(ready) {
-                allStats.forEach((stats, index) => {
-                    console.log(files[index], stats.isFile());
-                })
-            }
-        })
-    };
+        }
+
+    }
+
+    // Wokrs with method 1,2,3
+    // for(let file of files) {
+    //     try {
+    //         const stats = await lstat(file);
+
+    //         console.log(file, stats.isFile());
+    //     } catch (err){
+    //         console.log(err)
+    //     }
+    // }
 
 
 });
+
+// Method #1
+
+// const lstat = (files) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(files, (err, stats) => {
+//             if(err){
+//                 reject(err);
+//             }
+//             resolve(stats);
+//         })
+//     })
+// }
+    // const allStats = Array(files.length).fill(null);
+
+    // for (let file of files){
+    //     const index = files.indexOf(file)
+    //     fs.lstat(file, (err, stats) => {
+    //         if(err){
+    //             console.log(err);
+    //         }
+
+    //         allStats[index] = stats; 
+
+    //         const ready = allStats.every((stats) => {
+    //             return stats     
+    //         });
+
+    //         if(ready) {
+    //             allStats.forEach((stats, index) => {
+    //                 console.log(files[index], stats.isFile());
+    //             })
+    //         }
+    //     })
+    // };
+
+
 
 // const counterObject = require('./myscript.js')
 
